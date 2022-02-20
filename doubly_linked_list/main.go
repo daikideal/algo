9cde9dd8e39420ed5fafa2a3c5a37cbf3430945c
daikideal <54879050+daikideal@users.noreply.github.com>
2022-02-20 11:03:07 +0900

    Add: リンクリストを追加

---
+++ doubly_linked_list/main.go
@@ -0,0 +1,127 @@
+package main
+
+import (
+	"fmt"
+)
+
+type DoublyLinkedList struct {
+	Head *Node
+}
+
+type Node struct {
+	Data interface{}
+	Prev *Node
+	Next *Node
+}
+
+func (list *DoublyLinkedList) Append(data interface{}) {
+	newNode := &Node{Data: data}
+
+	if list.Head == nil {
+		list.Head = newNode
+		return
+	}
+
+	currentNode := list.Head
+	for currentNode.Next != nil {
+		currentNode = currentNode.Next
+	}
+
+	currentNode.Next = newNode
+	newNode.Prev = currentNode
+
+	return
+}
+
+func (list *DoublyLinkedList) Insert(data interface{}) {
+	newNode := &Node{Data: data}
+
+	if list.Head == nil {
+		list.Head = newNode
+		return
+	}
+
+	list.Head.Prev = newNode
+	newNode.Next = list.Head
+	list.Head = newNode
+
+	return
+}
+
+func (list *DoublyLinkedList) Remove(data interface{}) {
+	currentNode := list.Head
+
+	if currentNode != nil && currentNode.Data == data {
+		currentNode = nil
+		list.Head = currentNode.Next
+		return
+	}
+
+	for currentNode != nil && currentNode.Data != data {
+		currentNode = currentNode.Next
+	}
+
+	if currentNode == nil {
+		return
+	}
+
+	if currentNode.Next == nil {
+		previousNode := currentNode.Prev
+		previousNode.Next = nil
+		currentNode = nil
+		return
+	} else {
+		previousNode := currentNode.Prev
+		nextNode := currentNode.Next
+		previousNode.Next = nextNode
+		nextNode.Prev = previousNode
+		currentNode = nil
+		return
+	}
+}
+
+func (list *DoublyLinkedList) Reverse() {
+	previousNode := new(Node)
+	currentNode := list.Head
+
+	for currentNode != nil {
+		previousNode = currentNode.Prev
+		currentNode.Prev = currentNode.Next
+		currentNode.Next = previousNode
+
+		currentNode = currentNode.Prev
+	}
+
+	if previousNode != nil {
+		list.Head = previousNode.Prev
+	}
+
+	return
+}
+
+func (list DoublyLinkedList) Print() {
+	currentNode := list.Head
+
+	for currentNode != nil {
+		fmt.Println(currentNode.Data)
+		currentNode = currentNode.Next
+	}
+
+	return
+}
+
+func main() {
+	list := new(DoublyLinkedList)
+	list.Append(4)
+	list.Append(5)
+	list.Append(6)
+	list.Print()
+
+	fmt.Println("==> Reversing...")
+	list.Reverse()
+	list.Print()
+
+	fmt.Println("==> Deleting...")
+	list.Remove(5)
+	list.Print()
+}
---
+++ linked_list/main.go
@@ -0,0 +1,114 @@
+package main
+
+import (
+	"fmt"
+)
+
+type LinkedList struct {
+	Head *Node
+}
+
+type Node struct {
+	Data interface{}
+	Next *Node // 初期値をnilとする
+}
+
+// リストの終端にデータを追加する
+func (list *LinkedList) Append(data interface{}) {
+	newNode := &Node{Data: data}
+
+	if list.Head == nil {
+		list.Head = newNode
+		return
+	}
+
+	lastNode := list.Head
+	for lastNode.Next != nil {
+		lastNode = lastNode.Next
+	}
+	lastNode.Next = newNode
+	return
+}
+
+// リストの先端にデータを追加する
+func (list *LinkedList) Insert(data interface{}) {
+	newNode := &Node{Data: data}
+	newNode.Next = list.Head
+
+	list.Head = newNode
+	return
+}
+
+// 引数と最初に一致したデータをリストから削除する
+func (list *LinkedList) Remove(data interface{}) {
+	currentNode := list.Head
+
+	if currentNode != nil && currentNode.Data == data {
+		list.Head = currentNode.Next
+		currentNode = nil
+		return
+	}
+
+	previousNode := new(Node)
+	for currentNode != nil && currentNode.Data != data {
+		previousNode = currentNode
+		currentNode = currentNode.Next
+	}
+
+	if currentNode == nil {
+		return
+	}
+
+	previousNode.Next = currentNode.Next
+	currentNode = nil
+	return
+}
+
+func (list *LinkedList) Reverse() {
+	previousNode := new(Node)
+	currentNode := list.Head
+
+	for currentNode != nil {
+		nextNode := currentNode.Next
+		currentNode.Next = previousNode
+		// リストの終端が全フィールドがnilのNodeになることを防ぐ
+		if currentNode.Next.Data == nil {
+			currentNode.Next = nil
+		}
+		previousNode = currentNode
+		currentNode = nextNode
+	}
+	list.Head = previousNode
+
+	return
+}
+
+// リスト内のデータを全て出力する
+func (list LinkedList) Print() {
+	currentNode := list.Head
+
+	for currentNode != nil {
+		fmt.Println(currentNode.Data)
+		currentNode = currentNode.Next
+	}
+
+	return
+}
+
+func main() {
+	list := new(LinkedList)
+	list.Append(1)
+	list.Append(2)
+	list.Insert(3)
+	list.Print()
+
+	fmt.Println("==> Reversing...")
+	list.Reverse()
+	list.Print()
+
+	fmt.Println("==> Deleting...")
+	list.Remove(2)
+	list.Print()
+	list.Remove(5) // 何も起きない
+	list.Print()
+}
